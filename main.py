6138d063b75d8d4aa74833abae2dbf03fdb9127b
Jasminsofi <jasminsofi214@gmail.com>
2026-09-22 16:55:48 -0500

    Solucion de conflicto en el menu de main.py

--- main.py
+++ main.py
@@ -1,37 +1,32 @@
+from persistencia import *
+from inventario import *
+from prestamos import *
+
+inventario, prestamos = cargar_datos()
+
 while True:
 
-    try:
-        print("------------------------------------")
-        print("BIBLIOTECA HORIZONTE")   
-        print("Bienvenido a BiblioStock")
-        print("------------------------------------")
-        print("Seleccione la opcion segun lo que deseas realizar:\n")
-        opcion_menu=int(input(
-            "1.Registrar item\n" \
-            "2.Listar items\n" \
-            "3.Buscar item\n" \
-            "4.Registrar prestamo\n" \
-            "5.Registrar devolucion\n" \
-            "6.Salir\n "
-        ))
+    try: 
+        print("=" * 45)
+        print("     BIBLIOSTOCK CLI - BIBLIOTECA HORIZONTE")
+        print("=" * 45)
+        print("1. Registrar ítem\n2. Listar ítems\n3. Buscar ítem\n4. Registrar préstamo\n5. Registrar devolución\n6. Salir")
+        print("=" * 45)
+        opcion = input("Seleccione una opción: ").strip()
+        if opcion == "1":
+            registrar_item(inventario, prestamos)
 
-        if opcion_menu == 6:
-            print("Haz finalizado el programa, hasta pronto")
-            break
+        elif opcion == "2":
+            listar_items(inventario)
 
-        elif opcion_menu ==1:
-            #registrar_item()
-            pass
+        elif opcion == "3":
+            buscar_item(inventario)
 
-        elif opcion_menu ==2:
+        elif opcion == "4":
            pass
-        
 
-        elif opcion_menu ==3:
-            pass    
-
-        elif opcion_menu ==4:
-            pass    
+        elif opcion == "5":
+            pass
 
         elif opcion == "6":
             print("\nGracias por utilizar BiblioStock.")
@@ -41,4 +36,5 @@ while True:
             print("\nOpción inválida. Intente nuevamente.")
     except ValueError:
         print("Ingrese una opcion del menu correcta")
-       
+
+    
