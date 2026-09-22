9742aa097fa84cdad30ee71ec3ccc9ddf23b531c
Juan-1522 <jsuarez.e15@gmail.com>
2026-09-21 23:43:40 -0500

    Creé la rama prestamos.py y declaré una función

---
+++ prestamos.py
@@ -0,0 +1,4 @@
+def registrar_prestamo(inventario, prestamo):
+    codigo = input("Ingrese el código del ítem: ").strip()
+    usuario = input("Ingrese el nombre de usuario: ").strip()
+    fecha = input("Ingrese la fecha del prestamo: ").strip()
