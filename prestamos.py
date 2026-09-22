8d70c0c4c273b8da52e46081e6adcaedddc6af8f
Juan-1522 <jsuarez.e15@gmail.com>
2026-09-22 08:30:07 -0500

    Comencé a hacer la disponibilidad del item en prestamos

--- prestamos.py
+++ prestamos.py
@@ -2,3 +2,10 @@ def registrar_prestamo(inventario, prestamo):
     codigo = input("Ingrese el código del ítem: ").strip()
     usuario = input("Ingrese el nombre de usuario: ").strip()
     fecha = input("Ingrese la fecha del prestamo: ").strip()
+
+    for item in inventario:
+        if item["codigo"].lower() == codigo.lower():
+
+            if item["cantidad_disponible"] <= 0:
+                print("No hay unidades disponibles de este item")
+                return
