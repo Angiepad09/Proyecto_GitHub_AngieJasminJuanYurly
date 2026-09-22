26176bf85da34ed3950a1cfdb114609ccb990e0e
Juan-1522 <jsuarez.e15@gmail.com>
2026-09-22 17:05:12 -0500

    Corrección e implementación de préstamos y devoluciones

--- prestamos.py
+++ prestamos.py
@@ -12,22 +12,40 @@ def registrar_prestamo(inventario, prestamo):
 
             item["cantidad_disponible"] -= 1
 
-            prestamo = {
+            nuevo_prestamo = {
                 "codigo_item": item["codigo"],
                 "usuario": usuario,
                 "fecha": fecha,
                 "devuelto": False
             }
 
-            prestamos.append(prestamo)
+            prestamo.append(nuevo_prestamo)
 
             print("Prestamo registrado correctamente")
             return
 
-        print("No se encontró el item con ese código.")
+    print("No se encontró el item con ese código.")
 
 def registrar_devolucion(inventario, prestamos):
     codigo = input("Ingrese el código del Item: ").strip()
     usuario = input("Ingrese su nombre de usuario: ").strip()
 
-    
+    for prestamo in prestamos:
+        if (prestamo["codigo_item"].lower() == codigo.lower()
+                and prestamo["usuario"].lower() == usuario.lower):
+
+            if prestamo["devuelto"]:
+                print("Este préstamo ya fue devuelto")
+                return
+
+            prestamo["devuelto"] = True
+
+            for item in inventario:
+                if item["codigo"].lower() == codigo.lower():
+                    item["cantidad_disponible"] += 1
+                    break
+
+            print("Devolución registrada correctamente")
+            return
+
+    print("No se encontró un préstamo activo con estos datos.")
